9a607fa4b405620f4ccb45a0eb6e1c5b44be0dda
Marius Vicol <mariusvicol2004@gmail.com>
2024-10-20 17:20:23 +0300

    removing some stuff

--- src/main/java/ubb/scs/map/Main.java
+++ src/main/java/ubb/scs/map/Main.java
@@ -4,7 +4,6 @@ import static ubb.scs.map.StartApp.startApp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Social");
         startApp();
     }
 }
