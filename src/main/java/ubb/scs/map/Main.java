bfe796888167ed3b6d4cb22e127f5421b24a4964
Marius Vicol <mariusvicol2004@gmail.com>
2024-10-20 17:13:10 +0300

    adding a new things

--- src/main/java/ubb/scs/map/Main.java
+++ src/main/java/ubb/scs/map/Main.java
@@ -4,6 +4,7 @@ import static ubb.scs.map.StartApp.startApp;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Social");
         startApp();
     }
 }
